464a6d8f5b84148a345c7eefe5e703c18bf78ad5
Saurabh Khanwalkar <Saurabh_Khanwalkar@bose.com>
2017-06-12 14:30:04 -0400

    updated pom to skip tests updated url constants

--- src/main/java/com/gtranslate/URLCONSTANTS.java
+++ src/main/java/com/gtranslate/URLCONSTANTS.java
@@ -2,7 +2,7 @@ package com.gtranslate;
 
 public class URLCONSTANTS {
 	public static final String GOOGLE_TRANSLATE_TEXT = "http://translate.google.com/translate_a/t?";
-	public static final String GOOGLE_TRANSLATE_AUDIO = "http://translate.google.com/translate_tts?";
+	public static final String GOOGLE_TRANSLATE_AUDIO = "http://translate.google.com/translate_tts?tl=en&client=tw-ob&";
 	// v=1.0&q=alemao
 	public static final String GOOGLE_TRANSLATE_DETECT = "http://www.google.com/uds/GlangDetect?";
 
